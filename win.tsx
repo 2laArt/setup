e83b69c68d93ec11e628ca50ec4d6ce3f2544449
2laArt <88941287+2laArt@users.noreply.github.com>
2024-06-07 06:49:58 +0300

    Create win.tsx

---
+++ win.tsx
@@ -0,0 +1,10 @@
+power toys
+control panel / system and security / change user acount control setting - notification
+
+clipboard history : win + V
+
+preview files : alt + P
+
+always on top : win + ctrl + T
+
+delete unused imports : shift + alt + O
